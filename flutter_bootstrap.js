e88960c5ee6cdbcdd24e799c0433178c0106164f
Hallan Abreu <jotahagar@gmail.com>
2025-06-05 07:57:32 -0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "350567182"
+    serviceWorkerVersion: "3700869337"
   }
 });
